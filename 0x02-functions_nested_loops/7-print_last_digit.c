494d332d065a7559dc7ee4fa371838d790698015
Awe Ayo <pupoawe@gmail.com>
2022-09-13 15:49:01 +0100

    Create c program to print last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ * n: integer number
+ *
+ * Return: last digit of the number n
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,5 @@ void print_alphabet_x10(void);
 int _isalpha(int);
 int print_sign(int);
 int _abs(int);
+int print_last_digit(int)
 int _islower(int);
