180068df2e10ce5e7c9c8b85b11dc7edb61592a1
Awe Ayo <pupoawe@gmail.com>
2022-09-13 22:59:36 +0100

    Modify print_to_98 to meet requirements

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
-
+#include <string.h>
 #include <stdio.h>
 
+void print_number(int);
 /**
  * print_to_98 - counts from input n to 98
  * @n: starting number
@@ -18,8 +19,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98;
 				i++)
 		{
-			printf("%d", i);
-
+			print_number(i);
 			if (i != 98)
 			{
 				_putchar
@@ -35,7 +35,7 @@ void print_to_98(int n)
 		for (i = n; i >= 98;
 			i--)
 		{
-			printf("%d", i);
+			print_number(i);
 
 			if (i != 98)
 			{
@@ -50,3 +50,23 @@ void print_to_98(int n)
 	_putchar('\n');
 }
 
+
+/**
+ * print_number - prints numbers
+ * @n: number
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i;
+	char num[8];
+
+	sprintf(num, "%d", n);
+
+	for (i = 0; i < (int) strlen(num); i++)
+	{
+		_putchar(num[i]);
+	}
+}
