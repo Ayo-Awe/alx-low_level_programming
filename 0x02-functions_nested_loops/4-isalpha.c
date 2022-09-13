37a78fec387d915a8471f629dc66032061c7845b
Awe Ayo <pupoawe@gmail.com>
2022-09-13 14:39:54 +0100

    Create c program to solve isalpha task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is alphabetic
+ * @c: integer representing a character
+ *
+ * Return: 1 if character is alphabetic else 0
+ */
+
+int _isalpha(int c)
+{
+	if((c >= 97 && c <= 122) || (c >= 65 && c <=90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
