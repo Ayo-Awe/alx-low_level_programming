1a51c60bb2ef49cf68aeccf40d2cd8edf5f62c22
Awe Ayo <pupoawe@gmail.com>
2022-09-16 01:57:02 +0100

    Fix bugs in square printing function

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,13 @@
 
 void print_square(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			for (i = 1; i <= n; i++)
+			for (j = 1; j <= n; j++)
 			{
 				_putchar('#');
 			}
