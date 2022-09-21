e3ddedf1144f285ddda5e4a8f170b32ca426936f
Awe Ayo <pupoawe@gmail.com>
2022-09-21 13:46:51 +0100

    Create _strncat function

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatinating n bytes from source string with
+ * destination string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be concatenated
+ *
+ * Return: pointer string 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int start = strlen(dest);
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[start + i] = src[i];
+		i++;
+	}
+
+	dest[start + i] = '\0';
+
+	return (dest);
+}
