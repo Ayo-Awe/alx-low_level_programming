2cddef4d4ac1a137096c197f12a64baa052a6058
Awe Ayo <pupoawe@gmail.com>
2022-09-21 14:03:34 +0100

    Setup _strncpy function
    The _strncpy function works just like the strncpy function from the
    standard library

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copy n bytes from source string to
+ * destination string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be concatenated
+ *
+ * Return: pointer string 
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
