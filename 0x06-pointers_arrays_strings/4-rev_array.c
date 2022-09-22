46ce25c1ec8b96e73fc31154d23ccb689d91df1f
Awe Ayo <pupoawe@gmail.com>
2022-09-22 11:18:25 +0100

    Create revers_array function

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverses an array
+ * @arr: array
+ * @n: lenth of the array arr
+ *
+ * Return: void
+ */
+
+void  reverse_array(int *arr, int n)
+{
+	int i;
+	int temp;
+
+	n -= 1;
+	for (i = 0; i <= (n / 2); i++)
+	{
+		temp = arr[n - i];
+
+		arr[n - i] = arr[i];
+
+		arr[i] = temp;
+	}
+
+}
+
