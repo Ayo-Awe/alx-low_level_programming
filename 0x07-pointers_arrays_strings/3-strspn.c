725c564d2df8526cc55270cf06240c09dee2d16c
Awe Ayo <pupoawe@gmail.com>
2022-09-26 21:13:16 +0100

    Create _strspn script

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,60 @@
 #include "main.h"
 
+int contains(char *s, char c);
+
 /**
- * _strchr - finds the occurence of a char c
+ * _strspn - finds the occurence of a char c
  * @s: string
  * @accept: substring
  *
- * Return: pointers to occurence of c or NULL if none
+ * Return: number of bytes
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-    int i;
-    
-    i = 0;
+	int i;
+
+	i = 0;
 
 	while (s[i] != '\0')
 	{
+		if (contains(accept, s[i]))
+		{
+			i++;
+		}
+		else
+		{
+			/* If not first occurence of substring*/
+			if (i > 0)
+				return (i);
+
+			/* Else first occurence of substring*/
+			i++;
+		}
+	}
+
+	return (i);
+}
+
+/**
+ * contains - finds the occurence of a char c
+ * @s: string
+ * @c: string to find
+ *
+ * Return: pointers to occurence of c or NULL if none
+ */
+int contains(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
 		if (s[i] == c)
-			return (s+i);
+			return (1);
 		i++;
 	}
 
-	return (NULL);
+	return (0);
+
 }
