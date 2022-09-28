cccbb48e09c85820ef45669a3e8e36d415963e74
Awe Ayo <pupoawe@gmail.com>
2022-09-28 14:18:33 +0100

    Create _pow_recursion function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raises x to the power of y
+ * @x: base number
+ * @y: power
+ *
+ * Return: x to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
