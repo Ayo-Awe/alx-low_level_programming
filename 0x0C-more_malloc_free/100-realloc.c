4eb8a8d618302766aa90ed0fc4ee502fad566676
Awe Ayo <pupoawe@gmail.com>
2022-10-06 18:40:56 +0100

    Create _realloc function

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - creates new memory of new_size
+ * @ptr: number of elements
+ * @old_size: size of old memory in bytes
+ * @new_size: size of new memory in bytes
+ * Return: void pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	char *c_ptr, *c_p;
+	int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		return (p);
+	}
+
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		c_ptr = ptr;
+		c_p = p;
+
+		for (i = 0; i < (int) old_size; i++)
+		{
+			c_p[i] = c_ptr[i];
+		}
+	}
+	return (p);
+}
