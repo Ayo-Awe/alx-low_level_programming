a200a27381a18c4cff5888a98324f8def39cdfd6
Awe Ayo <pupoawe@gmail.com>
2022-10-06 16:39:59 +0100

    Create malloc_checked function

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - performs check before allocation memory
+ * @b: number of bytes
+ *
+ * Return: void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+
+}
