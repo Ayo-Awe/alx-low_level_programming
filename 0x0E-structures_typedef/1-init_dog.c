99e42b278d2ce437c72e62ea3b66333cf76e015e
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-11 11:11:47 +0100

    Add case for NULL pointer

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib>
 
 /**
  * init_dog - intialises the value of a dog struct
@@ -12,6 +13,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (dog == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
