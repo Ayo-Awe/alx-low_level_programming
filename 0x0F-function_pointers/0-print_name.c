bbe023b7c50c88f82c83abe931cc51bed2b232d4
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-13 07:48:10 +0100

    Check for null pointers in print_name function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
 * print_name - prints the name using a function pointer
@@ -9,5 +10,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
 	f(name);
 }
