eb7d6c67912a6e54dd85966d3b771d0a02f46bc0
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-13 08:26:54 +0100

    Create int_index function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+* int_index - returns the index of the first match in an array
+* @array: array to be iterated over
+* @size: size of array
+* @cmp: pointer to a compare function
+*
+* Return: index of match or -1 if otherwise
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp)
+		exit(1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
