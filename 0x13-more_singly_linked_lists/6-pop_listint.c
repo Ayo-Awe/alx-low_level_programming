fc02b99122357f1072ee9444ff6b5dcea55da8fe
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-25 22:57:16 +0100

    Create function to delete the head of a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * pop_listint - deletes the head of a list
+ * @head: a pointer to pointer to the address of head
+ *
+ * Return: the data stored in the previous head
+*/
+int pop_listint(listint_t **head)
+{
+	int data;
+	listint_t *new_head;
+
+	data = (*head)->n;
+	new_head = (*head)->next;
+
+	free(*head);
+
+	*head = new_head;
+
+	return (data);
+}
