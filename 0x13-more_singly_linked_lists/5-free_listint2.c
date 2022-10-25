89e816c04ba9625ba04682f14c7d203b1685f553
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-25 22:49:26 +0100

    Create function to free list and set head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_listint2 - frees a listint_t and sets head to null
+ * @head: pointer to pointer head of list
+ *
+ * Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	/* Set current node to head */
+	current = *head;
+
+	while (current)
+	{
+		/* Move on to next node */
+		next = current->next;
+
+		free(current);
+
+		current = next;
+	}
+
+	*head = NULL;
+
+}
