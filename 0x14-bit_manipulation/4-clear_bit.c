fb3ffd3ad06441ec78998fe5c163a2f1db448d36
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-28 05:08:20 +0100

    Add function to set bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * clear_bit - get the value of a bit
+ * @n: pointer to the integer to set bit
+ * @index: index of the bit value be retrieved
+ *
+ * Return: value of bit or -1 on error
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int mask = _pow(2, (int) index);
+	int and;
+
+	if (n == NULL)
+		return (-1);
+
+	/* get value at index, if and is 1 value is 1, vice versa*/
+	and = mask & *n;
+
+	if (index > 31)
+		return (-1);
+
+	if (and == mask)
+	{
+		printf("In here\n");
+		/* if bit at index is zero set it to 1*/
+		*n -= mask;
+		return (1);
+	}
+
+	/* if bit at index was alreay 1, return as is*/
+	return (1);
+}
+
+/**
+ * _pow - returns base to the power of exponent
+ * @base: integer representing base of exponent
+ * @exponent: integer representing exponent
+ *
+ * Return: base raised to the power of exponent
+*/
+int _pow(int base, int exponent)
+{
+	if (exponent == 0)
+	{
+		return (1);
+	}
+
+	return (base * _pow(base, exponent - 1));
+}
