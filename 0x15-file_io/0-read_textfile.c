399f0b598b1b1d1ab16a3b9d0b595a804a6a1dc0
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-31 21:01:03 +0100

    create function to read and print textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - read text from file
+ * @filename: name of file to be read
+ * @letters: number of letters to be read and printed
+ *
+ * Return: the number of letters read and printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int fd, rd, wr;
+
+	char *buf = malloc(sizeof(char) * (letters + 1));
+
+	if (!buf)
+		exit(1);
+
+	if (!filename)
+	{
+		return (0);
+		free(buf);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	rd = (int) read(fd, buf, letters);
+	if (rd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	wr = (int) write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (wr);
+}
+
+
