2780c89b6f1b8d535ce173d4c288a1312cacd33b
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-08 23:56:01 +0100

    fix: resolve bugs

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,7 +13,7 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *current = NULL;
 	size_t count = 0;
 
-	current = h;
+	current = (dlistint_t *)h;
 	while (current)
 	{
 		printf("%d\n", current->n);
