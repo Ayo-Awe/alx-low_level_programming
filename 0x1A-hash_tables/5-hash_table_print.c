72a6d32e0d0cb758af648589191def780d5fa7e7
Awe Ayomidipupo <pupoawe@gmail.com>
2023-01-06 00:34:24 +0100

    feat(0x1A): setup function to print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,68 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+void print_list(hash_node_t *head, unsigned int *print_count);
+void print_node(hash_node_t *node, unsigned int print_count);
+
+/**
+ * hash_table_print - prints out the elements of a hash table
+ * @ht: pointer to hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long i;
+	unsigned int print_count = 0;
+
+	if (!ht)
+		return;
+
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		print_list(ht->array[i], &print_count);
+	}
+	puts("}");
+}
+
+/**
+ * print_list - prints all the nodes in a linked list
+ * @head: head of linked list
+ *
+ * Return: void
+ */
+void print_list(hash_node_t *head, unsigned int *print_count)
+{
+	hash_node_t *current = NULL;
+
+	if (!head)
+		return;
+
+	current = head;
+
+	while (current)
+	{
+		print_node(current, *print_count);
+		*print_count += 1;
+		current = current->next;
+	}
+}
+
+/**
+ * print_node - prints the value of a single node in a linked list
+ * @node: pointer to node of linked list
+ *
+ * Return: void
+ */
+void print_node(hash_node_t *node, unsigned int print_count)
+{
+	if (!node)
+		return;
+
+	/* Print node in the required format */
+	if (print_count > 0)
+		printf(", '%s': '%s'", node->key, node->value);
+	else
+		printf("'%s': '%s'", node->key, node->value);
+}
